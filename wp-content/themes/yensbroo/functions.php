ee31fb764e9b07845ff8b902b2f3bc99ceb2a689
Yens Broothaers <yens.broothaers@student.arteveldehs.be>
2018-09-26 21:56:54 +0200

    change name of theme

--- wp-content/themes/yensbroo/functions.php
+++ wp-content/themes/yensbroo/functions.php
@@ -1,15 +1,15 @@
 <?php
 
-function scratch_enqueue_scripts() {
+function yensbroo_enqueue_scripts() {
   wp_enqueue_script( 'bootstrap', get_template_directory_uri() . '/bootstrap/js/bootstrap.bundle.js', array('jquery'));
   wp_enqueue_style( 'bootstrap', get_template_directory_uri() . '/bootstrap/css/bootstrap.min.css');
   wp_enqueue_style( 'style', get_stylesheet_uri());
 }
-add_action( 'wp_enqueue_scripts', 'scratch_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'yensbroo_enqueue_scripts' );
 
-function scratch_widgets_init() {
+function yensbroo_widgets_init() {
   register_sidebar( array(
-    'name' => __('Primary Sidebar', 'scratch'),
+    'name' => __('Primary Sidebar', 'yensbroo'),
     'id' => 'sidebar_primary',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget' => '</aside>',
@@ -17,7 +17,7 @@ function scratch_widgets_init() {
     'after_title' => '</h1>',
   ));
   register_sidebar( array(
-    'name' => __('Footer Sidebar 1', 'scratch'),
+    'name' => __('Footer Sidebar 1', 'yensbroo'),
     'id' => 'footer-sidebar-1',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget' => '</aside>',
@@ -25,7 +25,7 @@ function scratch_widgets_init() {
     'after_title' => '</h3>',
   ));
   register_sidebar( array(
-    'name' => __('Footer Sidebar 2', 'scratch'),
+    'name' => __('Footer Sidebar 2', 'yensbroo'),
     'id' => 'footer-sidebar-2',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget' => '</aside>',
@@ -33,7 +33,7 @@ function scratch_widgets_init() {
     'after_title' => '</h3>',
   ));
   register_sidebar( array(
-    'name' => __('Footer Sidebar 3', 'scratch'),
+    'name' => __('Footer Sidebar 3', 'yensbroo'),
     'id' => 'footer-sidebar-3',
     'before_widget' => '<aside id="%1$s" class="widget %2$s">',
     'after_widget' => '</aside>',
@@ -41,12 +41,12 @@ function scratch_widgets_init() {
     'after_title' => '</h3>',
   ));
 }
-add_action( 'widgets_init', 'scratch_widgets_init');
+add_action( 'widgets_init', 'yensbroo_widgets_init');
 
-add_action('after_setup_theme', 'scratch_setup');
-  if(! function_exists('scratch_setup') ):
-    function scratch_setup() {
-      register_nav_menu('primary', __( 'Primary navigation', 'scratch'));
+add_action('after_setup_theme', 'yensbroo_setup');
+  if(! function_exists('yensbroo_setup') ):
+    function yensbroo_setup() {
+      register_nav_menu('primary', __( 'Primary navigation', 'yensbroo'));
     } endif;
 
 require_once get_template_directory() . '/class-wp-bootstrap-navwalker.php';
